fe3c8b82020979025947354cc4b64318232ef8bb
Victor <huangv2@carleton.edu>
2021-01-22 02:11:27 -0500

    revised version of books.py

--- books/books.py
+++ books/books.py
@@ -1,14 +1,22 @@
 '''
 Author: Tianyi Lu, Victor Huang
+Revised by: Victor Huang
 Description: Gets arguments from users and then searchs for books matching user argument input
 Date: 2021-01-15 09:10:29
 LastEditors: Tianyi Lu
 LastEditTime: 2021-01-15 16:12:59
+
+REVISION SUMMARY:
+Revised some function names to match the purpose of said functions better. Changed some variable
+names following the "snake case" preference detailed in the Style Guide for Python Code. Made sure
+functions served a singular purpose simply and effectively. Added some comments to make understanding
+code easier. Followed the Style Guide as much as possible.
 '''
 
 import argparse
 import csv
 
+# Creats a book
 class Book():
     def __init__(self, title, year, author):
         self.title = title
@@ -22,6 +30,7 @@ class Book():
     def __eq__(self, other): 
         return self.__dict__ == other.__dict__
 
+# This function only gets arguments from the user
 def get_arguments():
     """
     Get argument from command line
@@ -41,11 +50,10 @@ def get_arguments():
 
     args = parser.parse_args()
 
-
     return args
 
-# 
-def read_file():
+# This function only loads the files from the books.csv file
+def load_books():
     """
     Load books from books.csv
     :return: A list of book objects
@@ -59,7 +67,7 @@ def read_file():
             
     return books
 
-def get_title(args, books):
+def get_books_by_title(args, books):
     """
     Get books whose titles contains the arguments
     :param args: args object containing all arguments
@@ -69,16 +77,16 @@ def get_title(args, books):
     if not args.title:
         return None
 
-    tlist = []
+    title_list = []
     for arg in args.title:
         for book in books:
             if arg.lower() in book.title.lower():
-                if not book in tlist:
-                    tlist.append(book)
-    return tlist              
+                if not book in title_list:
+                    title_list.append(book)
+    return title_list              
         
-
-def get_author(args, books):
+# This function only serves the purpose of getting books 
+def get_books_by_author(args, books):
     """
     Get books whose author name contains the arguments
     :param args: args object containing all arguments
@@ -89,23 +97,23 @@ def get_author(args, books):
     if not args.author:
         return None
 
-    adict = {}
+    author_dictionary = {}
     # Create a key value pair for every author that matches the arguments
     for arg in args.author:
         for book in books:
             if arg.lower() in book.author.lower():
-                if not book.author in adict.keys():
-                    adict[book.author] = []
+                if not book.author in author_dictionary.keys():
+                    author_dictionary[book.author] = []
 
     # Fill in the books written by every author in the dictionary
     for book in books:
-        if book.author in adict.keys():
-            adict[book.author].append(book)
+        if book.author in author_dictionary.keys():
+            author_dictionary[book.author].append(book)
 
     
-    return adict
+    return author_dictionary
 
-def get_year(args, books):
+def get_books_by_year(args, books):
     """
     Get books published between two year arguments
     :param args: args object containing all arguments
@@ -120,17 +128,18 @@ def get_year(args, books):
         args.year.pop()
         
     i = 0
-    ylist = []
+    year_list = []
     sortedYears = sorted(args.year)
     while i < (len(args.year) - 1):
         for book in books:
             if int(book.year) >= sortedYears[i] and int(book.year) <= sortedYears[i + 1]:
-                if not book in ylist:
-                    ylist.append(book)
+                if not book in year_list:
+                    year_list.append(book)
         i += 2
 
-    return ylist
-    
+    return year_list
+
+# This function only serves the purpose of finding the repeated values in the two lists
 def intersection(lst1, lst2):
     """
     Find the intersection of two lists
@@ -141,58 +150,59 @@ def intersection(lst1, lst2):
     lst3 = [value for value in lst1 if value in lst2] 
     return lst3
 
-def combine(tlist, adict, ylist):
+# This function only serves the purpose of combing all the results
+def combine(title_list, author_dictionary, year_list):
     """
-    Combine the results from get_title, get_year, and get_author. Print out the
+    Combine the results from get_books_by_title, get_books_by_year, and get_books_by_author. Print out the
     formated results
-    :param tlist: A list of book objects from get_title
-    :param adict: A dictionary of author and book objects from get_author
-    :param ylist: A list of book objects from get_year
+    :param title_list: A list of book objects from get_books_by_title
+    :param author_dictionary: A dictionary of author and book objects from get_books_by_author
+    :param year_list: A list of book objects from get_books_by_year
     """
-    # Intersection list of tlist and ylist
-    tylist = []
+    # Intersection list of title_list and year_list
+    title_year_list = []
 
-    only_adict = False
+    only_author_dictionary = False
 
     # If both title and year arguments appeared in the command line input
-    if (tlist is not None) and (ylist is not None):
-        tylist = intersection(tlist, ylist)
+    if (title_list is not None) and (year_list is not None):
+        title_year_list = intersection(title_list, year_list)
 
     # If only the title argument appeared in the command line input
-    elif tlist:
-        tylist = tlist
+    elif title_list:
+        title_year_list = title_list
 
     # If only the year argument appeared in the command line input
-    elif ylist:
-        tylist = ylist
+    elif year_list:
+        title_year_list = year_list
 
     # If only the author argument appeared in the command line input
     else:
-        only_adict = True
+        only_author_dictionary = True
 
-    if adict:
-        for author, books in adict.items():
-            if not only_adict:
-                adict[author] = intersection(books, tylist)
-        for author in adict.keys():
+    if author_dictionary:
+        for author, books in author_dictionary.items():
+            if not only_author_dictionary:
+                author_dictionary[author] = intersection(books, title_year_list)
+        for author in author_dictionary.keys():
             print()
             print(author+": ")
-            if not adict[author]:
+            if not author_dictionary[author]:
                 print( "\t(No book matches)")
             else:
-                for book in adict[author]:
+                for book in author_dictionary[author]:
                     print("\t" + str(book))
                 
     else:
-        print("\n%d books found\n" % len(tylist))
-        for i, book in enumerate(tylist):
+        print("\n%d books found\n" % len(title_year_list))
+        for i, book in enumerate(title_year_list):
             print(str(i+1)+". "+str(book))
 
 if __name__ == "__main__":
     args = get_arguments()
-    books = read_file()
-    tlist = get_title(args, books)
-    ylist = get_year(args, books)
-    adict = get_author(args, books)
-    combine(tlist, adict, ylist)
+    books = load_books()
+    title_list = get_books_by_title(args, books)
+    year_list = get_books_by_year(args, books)
+    author_dictionary = get_books_by_author(args, books)
+    combine(title_list, author_dictionary, year_list)
     
