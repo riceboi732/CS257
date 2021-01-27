61701b10db879f075560e13b5ca125f74bb3383d
Victor Huang <56984498+riceboi732@users.noreply.github.com>
2021-01-27 00:14:01 -0500

    Update database-schema.sql

--- database/database-schema.sql
+++ database/database-schema.sql
@@ -18,23 +18,6 @@ CREATE TABLE NOC_details (NOC text, region text, notes text);
 table6:
 CREATE TABLE olympians_olympics (name text, game text);
 
-TASK 1:
-SELECT DISTINCT noc FROM NOC_details ORDER BY noc;
-
-TASK 2:
-SELECT DISTINCT name FROM olympians  WHERE team = 'Kenya' ORDER BY name;
-
-TASK 3:
-SELECT olympians.name, olympics.games, sports_events_and_olympians.event, olympians.medals FROM olympics,sports_events_and_olympians,
-olympians,olympians_olympics WHERE olympians.name = 'Greg Louganis' AND olympians.name = olympians_olympics.name AND olympics.games = olympians_olympics.game AND medals IS NOT NULL AND olympians.name = sports_events_and_olympians.name ORDER BY year;
-
-TASK 4:
-SELECT noc, COUNT(olympians.medals = 'Gold') FROM olympians GROUP BY noc ORDER BY COUNT (olympians.medals = 'Gold') DESC;
-
-
-
-
-
 \copy olympians_olympics from 'olympians_olympics.csv' DELIMITER ',' CSV NULL AS 'NA';
 \copy olympians from 'olympians.csv' DELIMITER ',' CSV NULL AS 'NA';
 \copy olympics from 'olympics.csv' DELIMITER ',' CSV NULL AS 'NA';
