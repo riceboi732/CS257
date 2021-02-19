b9caeb902b8e9080153d7d33107605bb2816632f
victor <huangvictor2000@gmail.com>
2021-02-19 17:45:01 -0500

    final

--- webapp/doc/database-schema.sql
+++ webapp/doc/database-schema.sql
@@ -1,1 +1,20 @@
+CREATE TABLE states(
+	id int,
+	state_name string
+);
 
+
+CREATE TABLE victims(
+	id int,
+	victim_name string,
+	victim_ethnicity string,
+	victim_age int,
+	incident_date date,
+	armed_status boolean,
+	victim_gender string 
+);
+
+CREATE TABLE victim_state(
+	victim_id int,
+	state_id int
+)
