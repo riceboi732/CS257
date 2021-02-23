c1b481be7f94a169e058fd2345c51234d069ecfc
Martin Bernard <nawm@carleton.edu>
2021-02-23 15:36:08 -0800

    Finished /victims api

--- webapp/doc/api.py
+++ webapp/doc/api.py
@@ -33,19 +33,115 @@ def excute_query(cursor, query, check):
 
 @api.route('/victims')
 def get_victims():
-    state = flask.request.args.get('state', all)
-    min_year = int(flask.request.args.get('min_year', -10000))
-    max_year = int(flask.request.args.get('max_year', 10000))
+    state = flask.request.args.get('state', 'all')
+    min_year = int(flask.request.args.get('min_year', '1000'))
+    max_year = int(flask.request.args.get('max_year', '5000'))
     min_age = int(flask.request.args.get('min_age', -10000))
     max_age = int(flask.request.args.get('max_age', 10000))
-    ethnicity = flask.request.args.get('ethnicity', all)
-    armed = flask.request.args.get('armed', all)
-    
-    connection = connect_to_database
-    cursor = execute_querey(connection.cursor, query, check)
-    querey = "SELECT victims.incident_date, victims.victim_name, victims.victim_age, victims.victim_gender, victims.victim_ethnicity, victims.armed_status FROM victim_state, victims, states WHERE states.name = %s AND victims.id = victim_state.victim_id AND state.id = victim_state.state_id AND victim.incident_date >= min_year AND victim.incident_date <= max_year AND victim.victim_age <= max_age AND victim.victime_age >= min_year"
-    
+    ethnicity = flask.request.args.get('ethnicity', 'all')
+    armed = flask.request.args.get('armed', 'all')
 
+
+    check = (state, min_year, max_year, min_age, max_age, ethnicity, armed,)
+    
+    query = '''SELECT victims.incident_date, victims.victim_name, 
+                victims.victim_age, victims.victim_gender, victims.victim_ethnicity, victims.armed_status, states.state_name 
+                FROM victim_state, victims, states 
+                WHERE states.state_name = %s AND victims.id = victim_state.victim_id AND states.id = victim_state.state_id 
+                AND victims.victim_age >= %s AND victims.victim_age <= %s
+                AND victims.victim_ethnicity = %s 
+                AND victims.armed_status = %s 
+                AND victims.incident_date BETWEEN '01-01-%s' AND '12-31-%s' 
+                ORDER BY victims.incident_date;'''
+    
+    if state == 'all':
+        query = '''SELECT victims.incident_date, victims.victim_name, 
+                victims.victim_age, victims.victim_gender, victims.victim_ethnicity, victims.armed_status, states.state_name 
+                FROM victim_state, victims, states 
+                WHERE victims.victim_age >= %s AND victims.victim_age <= %s
+                AND victims.victim_ethnicity = %s 
+                AND victims.armed_status = %s 
+                AND victims.incident_date BETWEEN '01-01-%s' AND '12-31-%s' 
+                ORDER BY victims.incident_date;'''
+
+    elif state == 'all' and ethnicity == 'all':
+        query = '''SELECT victims.incident_date, victims.victim_name, 
+                victims.victim_age, victims.victim_gender, victims.victim_ethnicity, victims.armed_status, states.state_name 
+                FROM victim_state, victims, states 
+                WHERE victims.victim_age >= %s AND victims.victim_age <= %s
+                AND victims.armed_status = %s 
+                AND victims.incident_date BETWEEN '01-01-%s' AND '12-31-%s' 
+                ORDER BY victims.incident_date;'''
+
+    elif state == 'all' and armed == 'all':
+        query = '''SELECT victims.incident_date, victims.victim_name, 
+                victims.victim_age, victims.victim_gender, victims.victim_ethnicity, victims.armed_status, states.state_name 
+                FROM victim_state, victims, states 
+                WHERE victims.victim_age >= %s AND victims.victim_age <= %s
+                AND victims.victim_ethnicity = %s  
+                AND victims.incident_date BETWEEN '01-01-%s' AND '12-31-%s' 
+                ORDER BY victims.incident_date;'''
+
+    elif state == 'all' and ethnicity == 'all' and armed == 'all':
+        query = '''SELECT victims.incident_date, victims.victim_name, 
+                victims.victim_age, victims.victim_gender, victims.victim_ethnicity, victims.armed_status, states.state_name 
+                FROM victim_state, victims, states 
+                WHERE victims.victim_age >= %s AND victims.victim_age <= %s
+                AND victims.incident_date BETWEEN '01-01-%s' AND '12-31-%s' 
+                ORDER BY victims.incident_date;'''
+
+    elif ethnicity == 'all':
+        query = '''SELECT victims.incident_date, victims.victim_name, 
+                victims.victim_age, victims.victim_gender, victims.victim_ethnicity, victims.armed_status, states.state_name 
+                FROM victim_state, victims, states 
+                WHERE states.state_name = %s AND victims.id = victim_state.victim_id AND states.id = victim_state.state_id 
+                AND victims.victim_age >= %s AND victims.victim_age <= %s 
+                AND victims.armed_status = %s 
+                AND victims.incident_date BETWEEN '01-01-%s' AND '12-31-%s' 
+                ORDER BY victims.incident_date;'''
+
+    elif ethnicity == 'all' and armed == 'all':
+        query = '''SELECT victims.incident_date, victims.victim_name, 
+                victims.victim_age, victims.victim_gender, victims.victim_ethnicity, victims.armed_status, states.state_name 
+                FROM victim_state, victims, states 
+                WHERE states.state_name = %s AND victims.id = victim_state.victim_id AND states.id = victim_state.state_id 
+                AND victims.victim_age >= %s AND victims.victim_age <= %s
+                AND victims.incident_date BETWEEN '01-01-%s' AND '12-31-%s' 
+                ORDER BY victims.incident_date;'''
+
+    elif armed == 'all':
+        query = '''SELECT victims.incident_date, victims.victim_name, 
+                victims.victim_age, victims.victim_gender, victims.victim_ethnicity, victims.armed_status, states.state_name 
+                FROM victim_state, victims, states 
+                WHERE states.state_name = %s AND victims.id = victim_state.victim_id AND states.id = victim_state.state_id 
+                AND victims.victim_age >= %s AND victims.victim_age <= %s
+                AND victims.victim_ethnicity = %s
+                AND victims.incident_date BETWEEN '01-01-%s' AND '12-31-%s' 
+                ORDER BY victims.incident_date;'''
+
+
+    connection = connect_to_database()
+    cursor = excute_query(connection.cursor, query, check)
+
+    victims_list = []
+
+    for row in cursor:
+        date = row[0]
+        name = row[1]
+        age = row[2]
+        gender = row[3]
+        ethnicity = row[4]
+        armed = row[5]
+        state = row[6]
+
+        victims_list.append({'date': date, 'name': name, 'age': age, 'gender': gender, 'ethnicity': ethnicity, 'armed': armed, 'state': state})
+
+    connection.close()
+
+    return json.dump(victims_list)
+
+    
+    
 @api.route('/victims/analyze/year/<state>')
 def get_years_vis_data(state):
     pass
